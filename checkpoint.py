6b8cd38f69d686572bfc0ce5639795ddbe6b5c8a
BJ550 <bartojabase@gmail.com>
2023-08-08 15:49:39 -0300

    desarrollo HC

--- checkpoint.py
+++ checkpoint.py
@@ -12,7 +12,15 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+
+    if ((type(numero) != int) or (numero < 1)):
+        return None
+    elif numero == 1:
+        return numero
+    else:
+        return numero * Factorial(numero - 1)
+
+    #return 'Funcion incompleta'
 
 def EsPrimo(valor):
     '''
@@ -26,7 +34,17 @@ def EsPrimo(valor):
         EsPrimo(8) debe retornar False
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+
+    if type(valor) != int:
+        return None
+    
+    primo = True
+    for i in range(2,valor):
+        if valor%i == 0:
+            primo = False
+            break
+    return primo
+    #return 'Funcion incompleta'
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +65,16 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+
+    class Animal:
+        def __init__(self, especie, color):
+            self.edad = 0
+            self.especie = especie
+            self.color = color
+        
+        def CumplirAnios(self):
+            self.edad += 1
+            return self.edad
+    
+    return Animal(especie, color)
+    #return 'Funcion incompleta'
